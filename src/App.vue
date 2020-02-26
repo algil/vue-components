edc5679362b90a8be18d88a2b95db13fa9727912
Antonio Gil <antonioluisgil@gmail.com>
2020-02-26 21:14:02 +0100

    Configure project with prettier and reformat code

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,9 @@
 <template>
-  <div class="h-screen flex justify-center items-center">
+  <div class="flex items-center justify-center h-screen">
     <div class="w-16 h-16 bg-blue-500 rounded-full"></div>
   </div>
 </template>
 
 <script>
-export default {
-}
+export default {};
 </script>
